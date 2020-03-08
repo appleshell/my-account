dee333b4226ea9e55a39958984b318c2e5ae3ceb
Lvps <805697518@qq.com>
2020-03-08 22:40:16 +0800

    Button组件

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,17 +1,44 @@
 import React from 'react'
+import cn from 'classnames'
 import './style.scss'
 
 interface IProps {
   type?: string
   onClick?: React.MouseEventHandler<HTMLButtonElement>
+  className?: string
   children: React.ReactNode
+  [otherProps: string]: any
 }
 
 const Button: React.FC<IProps> = props => {
-  const { children } = props
-  return (
-  <button>{children}</button>
+  const { type, className, onClick, children, ...otherProps } = props
+
+  const compseClassName = cn(
+    'x-button',
+    `x-button--${type}`,
+    className,
   )
+
+  if(onClick) {
+    const clickHandle: React.MouseEventHandler<HTMLButtonElement> = evt => {
+      onClick(evt)
+    }
+    return (
+      <button className={compseClassName} onClick={clickHandle} {...otherProps}>
+        {children}
+      </button>
+    )
+  }
+
+  return (
+    <button className={compseClassName} {...otherProps}>
+      {children}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  type: 'primary'
 }
 
 export default Button
--- src/views/ui-demo/empty_line.tsx
+++ src/views/ui-demo/empty_line.tsx
@@ -3,10 +3,15 @@ import EmptyLine from '../../components/empty-line'
 
 const Demo = () => (
   <div>
-    <h3>组件名称：空行（EmptyLine）</h3>
-    <p>自定义组件，more高度20px，背景色#fff。</p>
+    <h1 style={{ fontSize: '24px' }}>组件名称：空行（EmptyLine）</h1>
+    <h2 style={{ fontSize: '20px' }}>基础用法</h2>
+    <p>自定义组件，默认高度20px，背景色#fff。</p>
     <p>第一行文字</p>
     <EmptyLine />
+    <p>第二行文字</p>
+    <h2 style={{ fontSize: '20px' }}>自定义高度和背景色</h2>
+    <p>第一行文字</p>
+    <EmptyLine height={28} background="#aaa" />
     <p>第二行文字</p>
   </div>
 )
