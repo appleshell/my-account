2aec6e2ef3af14d5dba32b2629795108156be228
Lvps <805697518@qq.com>
2020-03-16 23:29:43 +0800

    update

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './style.scss'
+import cn from 'classnames'
 
 interface IProps {
   className?: string
   placeholder?: string
   value: string
   disabled?: boolean
+  mini?: boolean
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
   inputProps?: React.InputHTMLAttributes<any>
   [otherProps: string]: any
@@ -14,11 +16,22 @@ interface IProps {
 // 开发组件：1、功能实现，2、样式，3、动画效果
 
 export default class Input extends React.PureComponent<IProps> {
+
+
+
   render() {
-    const { value, onChange, placeholder, inputProps, disabled } = this.props
+    const { className, value, onChange, placeholder, inputProps, disabled, mini } = this.props
+    const composeClassName = cn(
+      'x-input',
+      {
+        'x-input--mini': mini
+      },
+      className
+    )
     return (
       <input
         {...inputProps}
+        className={composeClassName}
         type="text"
         value={value}
         onChange={onChange}
--- src/views/ui-demo/input.tsx
+++ src/views/ui-demo/input.tsx
@@ -32,7 +32,12 @@ class Demo extends React.PureComponent<IState> {
         ></Input>
         <h2>禁用</h2>
         <p>设置disabled属性，input禁用</p>
-        <Input value={value} onChange={this.onChage} disabled />
+        <Input value={value} onChange={this.onChage} disabled placeholder="禁用状态" />
+        <h2>mini尺寸</h2>
+        <p>设置mini属性</p>
+        <Input value={value} onChange={this.onChage} mini placeholder="请输入" />
+        <h2>左右挂载</h2>
+        <h2>显示删除</h2>
       </div>
     )
   }
