0bfdc5051e4268c2ed44b67a30a5d3f37fa4189c
Lvps <805697518@qq.com>
2020-03-17 23:23:43 +0800

    update

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -10,34 +10,56 @@ interface IProps {
   mini?: boolean
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
   inputProps?: React.InputHTMLAttributes<any>
+  allowClear?: boolean
+  addonBefore?: React.ReactChild
+  addonAfter?: React.ReactChild
   [otherProps: string]: any
 }
 
 // 开发组件：1、功能实现，2、样式，3、动画效果
 
 export default class Input extends React.PureComponent<IProps> {
-
-
-
+  clearValue: React.EventHandler<any> = (e) => {
+    const {onChange, inputProps} = this.props
+    e.target = {value: '', ...inputProps}
+    onChange(e)
+  }
   render() {
-    const { className, value, onChange, placeholder, inputProps, disabled, mini } = this.props
+    const {
+      className,
+      value,
+      onChange,
+      placeholder,
+      inputProps,
+      disabled,
+      mini,
+      allowClear,
+      addonBefore,
+      addonAfter,
+    } = this.props
     const composeClassName = cn(
       'x-input',
       {
-        'x-input--mini': mini
+        'x-input--mini': mini,
+        'x-input--disabled': !!disabled,
       },
-      className
+      className,
     )
     return (
-      <input
-        {...inputProps}
-        className={composeClassName}
-        type="text"
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        disabled={!!disabled}
-      />
+      <div className={composeClassName}>
+        {addonBefore && <div className="x-input__addon-before">{addonBefore}</div>}
+        <input
+          {...inputProps}
+          className="x-input--ipt"
+          type="text"
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          disabled={!!disabled}
+        />
+        {(allowClear && !!value) && <span onClick={this.clearValue} className="x-input--clearIcon">×</span>}
+        {addonAfter && <div className="x-input__addon-after">{addonAfter}</div>}
+      </div>
     )
   }
 }
--- src/views/ui-demo/input.tsx
+++ src/views/ui-demo/input.tsx
@@ -25,11 +25,7 @@ class Demo extends React.PureComponent<IState> {
         <h1>组件名称：输入框（Input）</h1>
 
         <h2>基础用法</h2>
-        <Input
-          value={value}
-          onChange={this.onChage}
-          placeholder="请输入"
-        ></Input>
+        <Input value={value} onChange={this.onChage} placeholder="请输入"></Input>
         <h2>禁用</h2>
         <p>设置disabled属性，input禁用</p>
         <Input value={value} onChange={this.onChage} disabled placeholder="禁用状态" />
@@ -37,7 +33,22 @@ class Demo extends React.PureComponent<IState> {
         <p>设置mini属性</p>
         <Input value={value} onChange={this.onChage} mini placeholder="请输入" />
         <h2>左右挂载</h2>
+        <p>左右挂载的属性分别是addonBefore和addonAfter</p>
+        <Input
+          value={value}
+          onChange={this.onChage}
+          placeholder="请输入"
+          addonBefore={<span>√</span>}
+          addonAfter="RMB"
+        />
         <h2>显示删除</h2>
+        <p>设置allowClear属性为true</p>
+        <Input
+          value={value}
+          onChange={this.onChage}
+          placeholder="请输入"
+          allowClear
+        />
       </div>
     )
   }
