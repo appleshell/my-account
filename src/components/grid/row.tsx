546981a6846f21c78f049a22037dca302d61a32b
Lvps <805697518@qq.com>
2020-03-21 01:16:33 +0800

    grid组件

--- src/components/grid/row.tsx
+++ src/components/grid/row.tsx
@@ -3,19 +3,19 @@ import cn from 'classnames'
 import RowContext from './RowContext'
 
 interface RowProps extends React.HTMLAttributes<HTMLDivElement> {
-  gutter?: number | [number, number];
-  align?: string;
+  gutter?: number | [number, number]
+  align?: string
   justify?: string
 }
 
 export default class Row extends React.Component<RowProps> {
   static defaultProps = {
-    gutter: 0
+    gutter: 0,
   }
 
   GetGutter(): [number, number] {
-    let results: [number, number] = [0, 0];
-    const { gutter } = this.props;
+    let results: [number, number] = [0, 0]
+    const { gutter } = this.props
     const formatRes = Array.isArray(gutter) ? gutter : [gutter, 0]
     formatRes.forEach((g, index) => {
       results[index] = g || 0
@@ -23,17 +23,37 @@ export default class Row extends React.Component<RowProps> {
     return results
   }
 
-  renderRow = () => {
-    const {align, justify, className, style, children, ...otherProps} = this.props
+  render() {
+    const { align, justify, className, style, children, ...otherProps } = this.props
     const prefixCls = 'x-row'
     const gutter = this.GetGutter()
     const classString = cn(
       prefixCls,
       {
         [`${prefixCls}--justify`]: justify,
-        [`${prefixCls}--align`]: align
+        [`${prefixCls}--align`]: align,
       },
-      className
+      className,
+    )
+    const horiStyle = {
+      marginLeft: gutter[0]! / -2,
+      marginRight: gutter[0]! / -2,
+    }
+    const vertStyle = {
+      marginTop: gutter[1] / -2,
+      marginBottom: gutter[1] / 2,
+    }
+    const rowStyle = {
+      ...(gutter[0]! > 0 ? horiStyle : {}),
+      ...(gutter[1]! > 0 ? vertStyle : {}),
+      ...style,
+    }
+    return (
+      <RowContext.Provider value={{ gutter }}>
+        <div className={classString} style={rowStyle} {...otherProps}>
+          {children}
+        </div>
+      </RowContext.Provider>
     )
   }
 }
