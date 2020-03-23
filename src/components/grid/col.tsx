8ea016559b50c5b5988c30d7b379b2ae5339bffa
Lvps <805697518@qq.com>
2020-03-23 23:43:42 +0800

    update

--- src/components/grid/col.tsx
+++ src/components/grid/col.tsx
@@ -15,10 +15,39 @@ interface ColProps extends React.HTMLAttributes<HTMLDivElement> {
 
 class Col extends React.Component<ColProps, {}> {
   render() {
-    const { span, order, offset, push, pull, flex, className, style, children } = this.props
+    const { span, order, offset, push, pull, flex, className, style, children, ...ohterProps } = this.props
     const prefixCls = 'x-col'
+    const gridColumn = 24
     const classString = cn(prefixCls, className)
-    return <RowContext.Consumer>{value => <div>sd</div>}</RowContext.Consumer>
+    return (
+      <RowContext.Consumer>
+        {({ gutter }) => {
+          let colStyle: React.CSSProperties = { ...style }
+          const flexStyle = span ? {flex: `0 0 ${(Number(span) / gridColumn) * 100}%`} : {}
+          if (gutter) {
+            const horiStyle = {
+              paddingLeft: gutter[0]! / 2,
+              paddingRight: gutter[0]! / 2
+            }
+            const vertStyle = {
+              paddingTop: gutter[1]! / 2,
+              paddingBottom: gutter[1]! / 2
+            }
+            colStyle = {
+              ...(gutter[0]! > 0 ? horiStyle : {}),
+              ...(gutter[1]! > 0 ? vertStyle : {}),
+              ...flexStyle,
+              ...colStyle
+            }
+          }
+          return (
+            <div {...ohterProps} style={colStyle} className={classString}>
+              {children}
+            </div>
+          )
+        }}
+      </RowContext.Consumer>
+    )
   }
 }
 
--- src/views/ui-demo/grid.tsx
+++ src/views/ui-demo/grid.tsx
@@ -1,10 +1,114 @@
 import React from 'react'
 import './style.scss'
+import { Row, Col } from '../../components/grid'
 
-const Demo: React.SFC = () => (
+const Demo: React.SFC = () => {
+  const style = {
+    padding: '8px 0', background: '#63da55'
+  }
+  return (
   <div className="component-demo">
+    <h1>组件名称：Grid网格</h1>
+    <h2>基础用法：</h2>
+    <div style={{ marginBottom: '20px' }}>
+      <Row>
+        <Col span={6}>
+          <div style={style}>col-6</div>
+        </Col>
+        <Col span={6}>
+          <div style={style}>col-6</div>
+        </Col>
+        <Col span={6}>
+          <div style={style}>col-6</div>
+        </Col>
+        <Col span={6}>
+          <div style={style}>col-6</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={8}>
+          <div style={style}>col-8</div>
+        </Col>
+        <Col span={8}>
+          <div style={style}>col-8</div>
+        </Col>
+        <Col span={8}>
+          <div style={style}>col-8</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={12}>
+          <div style={style}>col-12</div>
+        </Col>
+        <Col span={12}>
+          <div style={style}>col-12</div>
+        </Col>
+      </Row>
+    </div>
+    <h2>设置区块间隔</h2>
+    <p>在Row组件上设置gutter属性。只为number时，表示水平间隔。值为数组时，分别表示水平间隔和垂直间隔</p>
+    <Row gutter={12}>
+      <Col span={8}>
+        <div style={style}>
+          col-8
+        </div>
+      </Col>
+      <Col span={8}>
+        <div style={style}>
+          col-8
+        </div>
+      </Col>
+      <Col span={8}>
+        <div style={style}>
+          col-8
+        </div>
+      </Col>
+    </Row>
+    <p>gutter为数组</p>
+    <Row gutter={[12,16]}>
+      <Col span={6}>
+        <div style={style}>
+          col-6
+        </div>
+      </Col>
+      <Col span={6}>
+        <div style={style}>
+          col-6
+        </div>
+      </Col>
+      <Col span={6}>
+        <div style={style}>
+          col-6
+        </div>
+      </Col>
+      <Col span={6}>
+        <div style={style}>
+          col-6
+        </div>
+      </Col>
+      <Col span={6}>
+        <div style={style}>
+          col-6
+        </div>
+      </Col>
+      <Col span={6}>
+        <div style={style}>
+          col-6
+        </div>
+      </Col>
+      <Col span={6}>
+        <div style={style}>
+          col-6
+        </div>
+      </Col>
+      <Col span={6}>
+        <div style={style}>
+          col-6
+        </div>
+      </Col>
+    </Row>
 
   </div>
-)
+)}
 
 export default Demo
