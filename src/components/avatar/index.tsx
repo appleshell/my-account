12e0c2948fbc85232a2f835106d4676c6115039f
Lvps <805697518@qq.com>
2020-03-27 23:53:31 +0800

    update

---
+++ src/components/avatar/index.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react'
+import './style.scss'
+import cn from 'classnames'
+
+interface AvatarProps {
+  shape?: 'circle' | 'square'
+  size?: 'large' | 'small' | 'default' | number
+  icon?: React.ReactNode
+  src?: string
+  srcSet?: string
+  alt?: string
+  style?: React.CSSProperties
+  className?: string
+  children?: React.ReactNode
+}
+
+interface AvatarState {
+  scale: number
+  mounted: boolean
+  isImgExist: boolean
+}
+
+class Avatar extends React.Component<AvatarProps, AvatarState> {
+  static defaultProps = {
+    shape: 'circle' as AvatarProps,
+    size: 'default' as AvatarProps,
+  }
+
+  render() {
+    const prefixCls = 'x-avator'
+    const { shape, size, icon, src, srcSet, alt, className, style, ...otherProps } = this.props
+
+    const classString = cn(
+      prefixCls,
+      {
+        [`${prefixCls}--lg`]: size === 'large',
+        [`${prefixCls}--sm`]: size === 'small',
+        [`${prefixCls}--${shape}`]: shape,
+        [`${prefixCls}--image`]: src,
+        [`${prefixCls}--icon`]: icon,
+      },
+      className,
+    )
+
+    const sizeStyle: React.CSSProperties =
+      typeof size === 'number'
+        ? {
+            height: size,
+            width: size,
+            lineHeight: `${size}px`,
+            fontSize: 18,
+          }
+        : {}
+
+    let { children } = this.props
+    if (src) {
+      children = <img src={src} alt={alt} />
+    } else if (icon) {
+      children = icon
+    } else {
+      children = <span className={`${prefixCls}--string`}>{children}</span>
+    }
+
+    return (
+    <span {...otherProps} className={classString} style={{...sizeStyle, ...style}}>{children}</span>
+    )
+  }
+}
+
+export default Avatar
