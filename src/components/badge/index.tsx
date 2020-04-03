ed969022415b435b7b991d1da4ea85cb4f456813
Lvps <805697518@qq.com>
2020-04-03 23:58:05 +0800

    init badge

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -1,2 +1,40 @@
 import * as React from 'react'
 import './style.scss'
+import cn from 'classnames'
+
+export interface BadgeProps {
+  count?: React.ReactNode;
+  status?: string
+  color?: string
+  text?: string
+  className?: string
+}
+
+class Badge extends React.Component<BadgeProps, any> {
+  hasStatus = (): boolean => {
+    const { status, color } = this.props
+    return !!status || !!color
+  }
+  getBadgeCls = (prefixCls: string) => {
+    const { className } = this.props
+    return cn(prefixCls, className)
+  }
+  render() {
+    const prefixCls = 'x-badge'
+    const { status, color, text, className } = this.props
+    const statusCls = cn({
+      [`${prefixCls}-status-dot`]: this.hasStatus(),
+      [`${prefixCls}-status-${status}`]: !!status,
+    })
+    const statusStyle: React.CSSProperties = {}
+    if(color) {
+      statusStyle.background = color
+    }
+    return <span className={this.getBadgeCls(prefixCls)}>
+      <span className={statusCls} style={statusStyle}></span>
+      <span className={`${prefixCls}-status-text`}>{text}</span>
+    </span>
+  }
+}
+
+export default Badge
