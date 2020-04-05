825be91f87765bdb7e41ee2e1db7a01741f8dd6f
Lvps <805697518@qq.com>
2020-04-06 00:21:36 +0800

    update

--- src/views/ui-demo/badge/statusDot.tsx
+++ src/views/ui-demo/badge/statusDot.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react'
-import '../style.scss'
+import './style.scss'
 import Badge from '../../../components/badge'
 
 const Demo: React.FC = () => {
   return (
     <div className="badge-demo badge-demo-dot">
       <h2>状态点</h2>
-      <Badge color="red" />
+      <p>设置status属性，展示状态</p>
+      <Badge status="default" />
+      <Badge status="success" />
+      <Badge status="processing" />
+      <Badge status="error" />
+      <Badge status="warning" />
+      <p>设置text属性，展示文本</p>
+      <Badge status="warning" text="Warning" />
+      <br/>
+      <Badge status="success" text="Success" />
     </div>
   )
 }
