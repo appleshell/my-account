058bf14677f489449ad7feb060f109c0db1fc2ae
Lvps <805697518@qq.com>
2020-04-13 23:52:43 +0800

    comment组件

---
+++ src/components/comment/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+interface CommentProps {}
+
+class Comment extends React.Component<CommentProps, {}> {
+  render() {
+    return <div>评论组件</div>
+  }
+}
+
+export default Comment
