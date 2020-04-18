463a4762f0a68ae7e4c758f3033abb0189c4a9af
Lvps <805697518@qq.com>
2020-04-18 23:59:16 +0800

    update

---
+++ src/views/index/components/aside.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Layout, Menu } from 'antd'
+
+const { Sider } = Layout
+
+class Aside extends React.PureComponent {
+  render() {
+    return (
+      <>
+        <Sider></Sider>
+      </>
+    )
+  }
+}
