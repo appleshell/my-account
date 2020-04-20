acffa7209abb30fef460aa19478e3db75a0d1413
lvpansen <pansen.lv@atzuche.com>
2020-04-20 14:06:01 +0800

    update

--- src/components/comment/index.tsx
+++ src/components/comment/index.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react'
+import './style.scss'
+import cn from 'classnames'
 
-interface CommentProps {}
+interface CommentProps {
+  content: React.ReactNode;
+  author?: React.ReactNode;
+  avatar?: React.ReactNode;
+  dateTime?: React.ReactNode;
+  className?: string
+}
 
 class Comment extends React.Component<CommentProps, {}> {
   render() {
-    return <div>评论组件</div>
+    const { content, author, avatar, dateTime, className, children } = this.props
+    const prefixCls = 'x-comment'
+    const classStrimg = cn(prefixCls, className)
+    return <div className={classStrimg}>评论组件</div>
   }
 }
 
