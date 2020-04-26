bfe8ae78ac92610de0e5c92f99dcc10ba7f6dc39
lvpansen <pansen.lv@atzuche.com>
2020-04-26 18:07:38 +0800

    add sentry

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import zh_CN from 'antd/lib/locale-provider/zh_CN'
 import Routes from './router'
 import './assets/style/index.css';
 import './assets/style/reset.css'
+import * as Sentry from '@sentry/browser'
+
+Sentry.init({ dsn: 'https://b25cb2b55cbd41668f20bfd73a42896b@o383632.ingest.sentry.io/5213916' })
 
 
 
 ReactDOM.render(
-    <ConfigProvider locale={zh_CN}>
-      <Routes/>
-    </ConfigProvider>
+  <ConfigProvider locale={zh_CN}>
+    <Routes />
+  </ConfigProvider>
   , document.getElementById('root'));
--- src/views/student/component/search.js
+++ src/views/student/component/search.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import {Input, Button} from 'antd'
+import * as Sentry from '@sentry/browser'
 
 class Search extends React.Component {
+
+  state = {
+    counter: 0,
+    obj: {}
+  }
   
   addStudent = () => {
     this.props.onShowModal()
+  }
+
+  test = () => {
+    const { counter } = this.state
+    this.setState({
+      counter: counter + 1,
+      obj: counter === 5 ? null : {}
+    })
+  }
+
+  componentDidCatch(error, errorInfo) {
+    Sentry.withScope((scope) => {
+        scope.setExtras(errorInfo);
+        const eventId = Sentry.captureException(error);
+        this.setState({eventId});
+        Sentry.showReportDialog({ eventId })
+    });
   }
 
   render() {
@@ -12,8 +35,9 @@ class Search extends React.Component {
       <div className="searchForm">
         <Input placeholder="请输入学生姓名"></Input>
         <Input placeholder="请输入学生学号"></Input>
-        <Button type="primary" icon="search">搜索</Button>
+        <Button type="primary" icon="search" onClick={this.test}>搜索</Button>
         <Button type="primary" icon="plus" onClick={this.addStudent}>新增</Button>
+        <span>{this.state.obj.sd}</span>
       </div>
     )
   }
